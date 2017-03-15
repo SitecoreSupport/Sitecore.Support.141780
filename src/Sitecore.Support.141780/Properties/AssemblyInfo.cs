bf278190bdd0049b89740f6a9377a0fc4e15cdea
Dmitry Baranov <dbr@sitecore.net>
2017-03-15 12:58:10 +0200

    Initial revision.

---
+++ src/Sitecore.Support.141780/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.141780")]
+[assembly: AssemblyProduct("Sitecore.Support.141780")]
+[assembly: ComVisible(false)]
